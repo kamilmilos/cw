4547344e4ff5c0ce70d83d758f32949072da7ba8
kamil.milos <kamil89666@gmail.com>
2019-04-13 18:17:00 +0200

    First version of the project

--- kolkokrzyzyk/Program.cs
+++ kolkokrzyzyk/Program.cs
@@ -10,6 +10,21 @@ namespace kolkokrzyzyk
     {
         static void Main(string[] args)
         {
+            string imiegraczaA = "";
+            string imiegraczaB = "";
+            char znakgraczaA = 'x';
+            char znakgraczaB = 'o';
+            char[,]
+plansza = new char[3, 3] {
+{'1','2','3'},
+{ '4','5','6'},
+ { '7','8', '9'} };
+        Console.Write("wpisz imie gracza A: ");
+            imiegraczaA = Console.ReadLine();
+            Console.Write("wpisz imie gracza B: ");
+            imiegraczaB = Console.ReadLine();
+
+
         }
     }
 }
